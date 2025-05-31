f502620c9fc551ead1d0e1b2baa0b14ad52a59a0
basedgaming2 <dominicnastuk2@gmail.com>
2025-05-30 20:47:56 -0400

    Update Govee.js

--- Govee.js
+++ Govee.js
@@ -980,7 +980,7 @@ const GoveeDeviceLibrary = {
 		state: 1,
 		supportRazer: true,
 		supportDreamView: true,
-		ledCount: 10
+		ledCount: 8
 	},
 	H619D: {
 		name: "2*7.5m RGBIC Pro Strip Lights",
